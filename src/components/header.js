11f8195f8eddb4503b824cb4c7a5bc8e048fcb8b
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-11-08 03:28:34 -0800

    google authentication complete

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,34 @@
-import { auth } from "../../auth";
+import Link from "next/link";
+import { auth, signOut } from "../../auth";
 
-export default async function Header(){
-    const session = await auth();
-    console.log("session", session);
-    return(
-        <div className="w-full bg-black">
-            <h1 className="text-white">Hello World</h1>
-            <button>Login</button>
-
+export default async function Header() {
+  const session = await auth();
+  console.log("session", session);
+  return (
+    <div className="w-full flex justify-between px-4 items-center bg-black py-4">
+      <h1 className="text-white text-2xl">Hello World</h1>
+      {session ? (
+        <div className="flex gap-4 items-center">
+          <h2 className="text-white">{session?.user?.email}</h2>
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+          >
+            <button
+              type="submit"
+              className="h-10 w-28 text-dark bg-white rounded"
+            >
+              LogOut
+            </button>
+          </form>
         </div>
-    )
+      ) : (
+        <Link href={'/Login'}>
+        <button className="h-10 w-28 text-dark bg-white rounded">Login</button>
+         </Link>
+      )}
+    </div>
+  );
 }
